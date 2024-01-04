0f8b623d1d4335d62ae375500c7ee3f870de982e
IuryChristmas <iury.christmas@gmail.com>
2024-01-04 08:22:25 -0300

    create person service unit test

---
+++ src/test/java/br/com/erudio/unittests/service/PersonServiceTest.java
@@ -0,0 +1,57 @@
+package br.com.erudio.unittests.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
+import br.com.erudio.service.PersonService;
+import br.com.erudio.unittests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.util.Optional;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @BeforeEach
+    void setUpMocks() throws Exception {
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindById() {
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+
+        when(personRepository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = personService.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", result.getAddress());
+        assertEquals("First Name Test1", result.getFirstName());
+        assertEquals("Last Name Test1", result.getLastName());
+        assertEquals("Female", result.getGender());
+    }
+}
