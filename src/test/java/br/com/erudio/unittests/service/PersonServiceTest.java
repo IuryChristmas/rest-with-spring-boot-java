7510bbf3201f6f24c7f8a3d7ad456d56f81e711b
IuryChristmas <iury.christmas@gmail.com>
2024-01-09 08:47:25 -0300

    finish unit tests and replace dozer for modelmapper

--- src/test/java/br/com/erudio/unittests/service/PersonServiceTest.java
+++ src/test/java/br/com/erudio/unittests/service/PersonServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.erudio.unittests.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import br.com.erudio.data.vo.v1.PersonVO;
@@ -18,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -58,15 +60,13 @@ public class PersonServiceTest {
 
     @Test
     void testCreate() {
-        Person entity = input.mockEntity(1);
-
-        Person persisted = entity;
+        Person persisted = input.mockEntity(1);
         persisted.setId(1L);
 
         PersonVO vo = input.mockVO(1);
         vo.setKey(1L);
 
-        when(personRepository.save(entity)).thenReturn(persisted);
+        when(personRepository.save(any(Person.class))).thenReturn(persisted);
 
         var result = personService.create(vo);
         assertNotNull(result);
@@ -134,5 +134,46 @@ public class PersonServiceTest {
         when(personRepository.findById(1L)).thenReturn(Optional.of(entity));
 
         personService.delete(1L);
+    }
+
+    @Test
+    void testFindAll() {
+        List<Person> list = input.mockEntityList();
+
+        when(personRepository.findAll()).thenReturn(list);
+
+        var people = personService.findAll();
+        assertNotNull(people);
+        assertEquals(14, people.size());
+
+        var personOne = people.get(1);
+
+        assertNotNull(personOne.getKey());
+        assertNotNull(personOne.getLinks());
+        assertTrue(personOne.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+        assertEquals("Addres Test1", personOne.getAddress());
+        assertEquals("First Name Test1", personOne.getFirstName());
+        assertEquals("Last Name Test1", personOne.getLastName());
+        assertEquals("Female", personOne.getGender());
+
+        var personFour = people.get(4);
+
+        assertNotNull(personFour.getKey());
+        assertNotNull(personFour.getLinks());
+        assertTrue(personFour.toString().contains("links: [</api/person/v1/4>;rel=\"self\"]"));
+        assertEquals("Addres Test4", personFour.getAddress());
+        assertEquals("First Name Test4", personFour.getFirstName());
+        assertEquals("Last Name Test4", personFour.getLastName());
+        assertEquals("Male", personFour.getGender());
+
+        var personSeven = people.get(7);
+
+        assertNotNull(personSeven.getKey());
+        assertNotNull(personSeven.getLinks());
+        assertTrue(personSeven.toString().contains("links: [</api/person/v1/7>;rel=\"self\"]"));
+        assertEquals("Addres Test7", personSeven.getAddress());
+        assertEquals("First Name Test7", personSeven.getFirstName());
+        assertEquals("Last Name Test7", personSeven.getLastName());
+        assertEquals("Female", personSeven.getGender());
     }
 }
