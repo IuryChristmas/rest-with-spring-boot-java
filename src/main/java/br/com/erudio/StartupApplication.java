eb8f27102b620cf5fc2a35663400448e810db6d2
IuryChristmas <iury.christmas@gmail.com>
2024-05-06 08:46:34 -0300

    start implement security config but need refactor to work in actual version

--- src/main/java/br/com/erudio/StartupApplication.java
+++ src/main/java/br/com/erudio/StartupApplication.java
@@ -2,12 +2,26 @@ package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class StartupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartupApplication.class, args);
+
+		/*Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("bcrypt", new BCryptPasswordEncoder());
+		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt", encoders);
+		passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+
+		String result = passwordEncoder.encode("admin234");
+		System.out.println("My hash " + result);*/
 	}
 
 }
---
+++ src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.erudio.config;
+
+import br.com.erudio.security.jwt.JwtConfigurer;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> enconders = new HashMap<>();
+        enconders.put("bcrypt", new BCryptPasswordEncoder());
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("bcrypt", enconders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(new BCryptPasswordEncoder());
+        return passwordEncoder;
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .disable()
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/signing", "/auth/refresh", "/api-docs/**", "/swagger-ui.html**").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .requestMatchers("/users").denyAll()
+                .and()
+                .cors()
+                .and()
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .apply(new JwtConfigurer(jwtTokenProvider));
+
+    }
+
+}
