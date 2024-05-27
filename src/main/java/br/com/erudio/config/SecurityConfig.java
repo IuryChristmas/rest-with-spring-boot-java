033965c58e2df266ba3a134a04eb7946259e105d
IuryChristmas <iury.christmas@gmail.com>
2024-05-27 19:31:29 -0300

    add controller and verification method to login

--- src/main/java/br/com/erudio/config/SecurityConfig.java
+++ src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,7 +32,7 @@ public class SecurityConfig {
         return passwordEncoder;
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
+    /*protected void configure(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .disable()
@@ -46,6 +47,18 @@ public class SecurityConfig {
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .apply(new JwtConfigurer(jwtTokenProvider));
 
+    }*/
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(request -> request
+                        .requestMatchers("/auth/signing", "/auth/refresh", "/api-docs/**", "/swagger-ui.html**")
+                        .permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/users").denyAll())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> new JwtConfigurer(jwtTokenProvider))
+                .build();
     }
 
 }
